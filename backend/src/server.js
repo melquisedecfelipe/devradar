db6ac2966afad73560588b406384d0f92cd2892f
Melquisedec Felipe <melquisedecfelipe@gmail.com>
2020-01-23 10:14:57 -0200

    add socket config

--- backend/src/server.js
+++ backend/src/server.js
@@ -1,10 +1,16 @@
 const express = require('express');
 const mongoose = require('mongoose');
 const cors = require('cors');
-const routes = require('./routes');
+const http = require('http');
 require('dotenv').config();
 
+const routes = require('./routes');
+const { setupWebsocket } = require('./websocket');
+
 const app = express();
+const server = http.Server(app);
+
+setupWebsocket(server);
 
 mongoose.connect(process.env.MONGO_URL, {
   useNewUrlParser: true,
